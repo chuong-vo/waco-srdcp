b98cb8858a69885f64016183e2ee809dc01c1eb1
chuong-vo <nguyenchuong0618@gmail.com>
2025-10-12 14:39:50 +0000

    Restore z1 sky platform

--- examples/simulation/waco-srdcp/project-conf.h
+++ examples/simulation/waco-srdcp/project-conf.h
@@ -6,26 +6,40 @@
 /* ========================================================================== */
 
 /* RF channel 26 for CC2420 */
+#ifndef CC2420_CONF_CHANNEL
 #define CC2420_CONF_CHANNEL 26
+#endif
 /* Network's PAN ID */
+#ifndef IEEE802154_CONF_PANID
 #define IEEE802154_CONF_PANID 0xABCD
+#endif
 /* Use hardware auto-acknowledgements */
+#ifndef CC2420_CONF_AUTOACK
 #define CC2420_CONF_AUTOACK 1
+#endif
 /* Let wurrdc use the 802.15.4 auto-ack feature */
+#ifndef WURRDC_CONF_802154_AUTOACK
 #define WURRDC_CONF_802154_AUTOACK 1
+#endif
 
 /* ========================================================================== */
 /* ========================= Network Stack Settings ========================= */
 /* ========================================================================== */
 
 /* --- MAC Layer --- */
+#ifndef NETSTACK_CONF_MAC
 #define NETSTACK_CONF_MAC csma_driver
+#endif
 
 /* --- Framer --- */
+#ifndef NETSTACK_CONF_FRAMER
 #define NETSTACK_CONF_FRAMER framer_802154
+#endif
 
 /* --- Link-layer Security --- */
+#ifndef NETSTACK_CONF_LLSEC
 #define NETSTACK_CONF_LLSEC nullsec_driver
+#endif
 
 /* --- RDC (Radio Duty Cycling) Layer --- */
 /* By default, wurrdc_driver is used.
@@ -46,10 +60,13 @@
 #endif
 
 /* Enable Rime statistics to count retransmissions/ACKs */
+#ifndef RIMESTATS_CONF_ENABLED
 #define RIMESTATS_CONF_ENABLED 1
+#endif
 
 /* Disable IPv6 for Rime-based applications */
-#undef UIP_CONF_IPV6
+#ifndef UIP_CONF_IPV6
 #define UIP_CONF_IPV6 0
+#endif
 
 #endif /* PROJECT_CONF_H_ */
